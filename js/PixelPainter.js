8f5b31ab2484299b02bf32b4dff2074dba034924
Reyn-L <Reyn.Leong@gmail.com>
2017-06-20 10:59:48 -1000

    apphend child new divs and changed variables

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,48 +1,64 @@
 /* jshint esversion: 6*/
+let pixelPainter = document.querySelector("#pixelPainter");
 
-let colors = [
-{hex : '#000'},
-{hex : '#fff'},
-{hex : '#ff0000'},
-{hex: '#ffb6c1'},
-{hex: '#66cdaa'}
+let title = document.createElement("div");
+title.id = "title";
+let leftDiv = document.createElement("div");
+leftDiv.id = "leftDiv";
+let rightDiv = document.createElement("div");
+rightDiv.id = "rightDiv";
+pixelPainter.appendChild(title);
+title.appendChild(leftDiv);
+title.appendChild(rightDiv);
+
+let colors = ['#000',
+'#fff',
+'#ff0000',
+'#ffb6c1',
+'#66cdaa'
 ];
 
-let pixelPainter = document.getElementById("pixelPainter");
-function generateGrid (numCols, numCells){
+function generateGrid (numRows, numCells, nodeAttribute){
+  function name (node, attribute){
+    let atr = Object.keys(attribute);
+    for ( let i = 0; i < atr.length; i++){
+      node.setAttribute(atr[i], attribute[atr[i]]);
+    }
+  }
+
   let grid = document.createElement("div");
   grid.className = "grid";
-  for(let i = 0; i < numCols; i++){
-    let cols = document.createElement("div");
-    grid.appendChild(cols);
-    cols.className = "cols";
+  for(let i = 0; i < numRow; i++){
+    let rows = document.createElement("div");
+    grid.appendChild(rows);
+    rows.className = "rows";
     for(let j = 0; j < numCells; j++){
       let cells = document.createElement("div");
       cells.className = "cells";
-      cols.appendChild(cells);
+      rows.appendChild(cells);
     }
   }
-  pixelPainter.appendChild(grid);
+  return grid;
 }
 
 
-function generateColorGrid (numColorCols, numColorCells){
+function generateColorGrid (numColorRows, numColorCells){
   let colorGrid = document.createElement("div");
   colorGrid.className = "colorGrid";
-  for(let i = 0; i < numColorCols; i++){
-    let colorCols = document.createElement("div");
-    colorGrid.appendChild(colorCols);
-    colorCols.className = "colorCols";
+  for(let i = 0; i < numColorRows; i++){
+    let colorRows = document.createElement("div");
+    colorGrid.appendChild(colorRows);
+    colorRows.className = "colorRows";
     for(let j = 0; j < numColorCells; j++){
       let colorCells = document.createElement("div");
       colorCells.className = "colorCells";
       colorCells.id = j;
       colorCells.setAttribute('style', 'background-color:' + colors[i].hex);
-      colorCols.appendChild(colorCells);
+      colorRows.appendChild(colorCells);
     }
   }
-  pixelPainter.appendChild(colorGrid);
+  leftDiv.appendChild(colorGrid);
 }
 
 generateColorGrid(5,1);
-generateGrid(5,5);
+//generateGrid(5,5);
