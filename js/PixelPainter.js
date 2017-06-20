7708aa1c4a73d81bfbc35f5b4d5c6926d13fceed
liznomura <lizn@hawaii.edu>
2017-06-19 22:06:41 -1000

    slowly making progress

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,19 +1,48 @@
 /* jshint esversion: 6*/
-let pixelPainter = document.getElementById("pixelPainter");
-function generateGrid (numRows, numCols){
-  let grid = document.createElement("div");
 
-  //document.getElementById("div").className = "grid";
-  for(let i = 0; i < numRows; i++){
-    let rows = document.createElement("div");
-    grid.appendChild(rows);
-    //rows.className = "rows";
-    for(let j = 0; j < numCols; j++){
+let colors = [
+{hex : '#000'},
+{hex : '#fff'},
+{hex : '#ff0000'},
+{hex: '#ffb6c1'},
+{hex: '#66cdaa'}
+];
+
+let pixelPainter = document.getElementById("pixelPainter");
+function generateGrid (numCols, numCells){
+  let grid = document.createElement("div");
+  grid.className = "grid";
+  for(let i = 0; i < numCols; i++){
+    let cols = document.createElement("div");
+    grid.appendChild(cols);
+    cols.className = "cols";
+    for(let j = 0; j < numCells; j++){
       let cells = document.createElement("div");
-      //cells.className = "cells";
-      rows.appendChild(cells);
+      cells.className = "cells";
+      cols.appendChild(cells);
     }
   }
   pixelPainter.appendChild(grid);
 }
-generateGrid(5,4);
+
+
+function generateColorGrid (numColorCols, numColorCells){
+  let colorGrid = document.createElement("div");
+  colorGrid.className = "colorGrid";
+  for(let i = 0; i < numColorCols; i++){
+    let colorCols = document.createElement("div");
+    colorGrid.appendChild(colorCols);
+    colorCols.className = "colorCols";
+    for(let j = 0; j < numColorCells; j++){
+      let colorCells = document.createElement("div");
+      colorCells.className = "colorCells";
+      colorCells.id = j;
+      colorCells.setAttribute('style', 'background-color:' + colors[i].hex);
+      colorCols.appendChild(colorCells);
+    }
+  }
+  pixelPainter.appendChild(colorGrid);
+}
+
+generateColorGrid(5,1);
+generateGrid(5,5);
